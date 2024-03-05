c2c08b82740e16ea20562542c16c97a4561dee0e
Niranjanee B <107741629+Niranjanee-10@users.noreply.github.com>
2024-03-05 13:57:06 +0530

    Add files via upload

---
+++ Back_End/Day 10/src/test/java/com/birthdayevent/niranjanee/NiranjaneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birthdayevent.niranjanee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiranjaneeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
